5bdc25cd2c8c481a79661bdd037d21b0221c64d5
james <engrjayt200@gmail.com>
2022-01-28 15:53:30 +0100

    Add config file

--- client/src/config/serverApiConfig.js
+++ client/src/config/serverApiConfig.js
@@ -1,10 +1,10 @@
  import axios from 'axios';
  export const axiosInstance = axios.create({
-   baseURL: 'http://localhost:8888/api/',
+   baseURL: 'https://salesflare-dashboard.herokuapp.com/api/',
  });
  
  
- export const API_BASE_URL = "http://localhost:8888/api/";
+ export const API_BASE_URL = 'https://salesflare-dashboard.herokuapp.com/api/';
 // export const ACCESS_TOKEN_NAME = "x-auth-token";
 // export const DOWNLOAD_BASE_URL = "http://localhost:8888/download/";
 //export const API_BASE_URL = 'use this when deploy';
