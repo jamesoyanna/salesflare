998b8520b6483f7ab13e0374605747dd3772b80c
james <engrjayt200@gmail.com>
2022-01-29 15:48:42 +0100

    Implement Redux for context for state management

--- client/src/config/serverApiConfig.js
+++ client/src/config/serverApiConfig.js
@@ -1,9 +1,5 @@
-//  import axios from 'axios';
-//  export const axiosInstance = axios.create({
-//    baseURL: 'https://salesflare-dashboard.herokuapp.com/api/',
-//  });
- 
- 
+
+ //export const API_BASE_URL = "http://localhost:8888/api/"; 
  export const API_BASE_URL = 'https://salesflare-dashboard.herokuapp.com/api/';
 // export const ACCESS_TOKEN_NAME = "x-auth-token";
 // export const DOWNLOAD_BASE_URL = "http://localhost:8888/download/";
