76df2696ef98ace06c8af637406a244a0c43912b
James <engrjayt200@gmail.com>
2022-08-27 20:54:28 +0100

    Update

--- client/src/config/serverApiConfig.js
+++ client/src/config/serverApiConfig.js
@@ -1,12 +1,10 @@
-    // export const API_BASE_URL = 'https://salesflare-backend.herokuapp.com/api/';
- // export const API_BASE_URL = "http://localhost:8888/api/"; 
- export const API_BASE_URL = 'https://salesflare-backend-app.herokuapp.com/api/';
+     export const API_BASE_URL = 'https://flare-sales.herokuapp.com/api/';
+  //export const API_BASE_URL = "http://localhost:8888/api/"; 
+ //export const API_BASE_URL = 'https://salesflare-backend.herokuapp.com/api/';
 // export const ACCESS_TOKEN_NAME = "x-auth-token";
 // export const DOWNLOAD_BASE_URL = "http://localhost:8888/download/";
 //export const API_BASE_URL = 'use this when deploy';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 export const DOWNLOAD_BASE_URL = '';
-
-
 
 
